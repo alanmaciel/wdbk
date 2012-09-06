9464dffc1d0474313e02a661fd4b95e068fd685b
Alan Maciel <alan@ubernetics.com>
2012-09-06 12:45:13 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wdbk::Application.config.secret_token = '1b53654a3dbd0096b3d13bce4b62e4b73947abafd32fc8fcbb8ffd6e30c08120803be2413e834082c6338a93f6dc02dc97c411c1281078385978211d2ae37db9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wdbk::Application.config.session_store :cookie_store, key: '_wdbk_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wdbk::Application.config.session_store :active_record_store
